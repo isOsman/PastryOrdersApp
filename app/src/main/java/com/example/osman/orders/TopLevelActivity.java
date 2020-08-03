b515c3873f0f9c9742c81ba0334f558ceff1a758
Osman <niyazovo@inbox.ru>
2020-08-03 13:42:02 +0600

    Modified SplashActivity Modified TopLevelActivity Added buttons Added styles

--- app/src/main/java/com/example/osman/orders/TopLevelActivity.java
+++ app/src/main/java/com/example/osman/orders/TopLevelActivity.java
@@ -1,7 +1,9 @@
 package com.example.osman.orders;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+    }
+
+    public void btnClick(View view) {
+        if(view.getId() == R.id.orderBtn){
+            startActivity(new Intent(this,MainActivity.class));
+        }
     }
 }
