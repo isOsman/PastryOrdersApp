45fc35c2746948e8feac2cb83675c3c0e081339e
Osman <niyazovo@inbox.ru>
2020-08-08 17:59:46 +0600

    Modified checkTips():interval switched to the Day

--- app/src/main/java/com/example/osman/orders/TipActivity.java
+++ app/src/main/java/com/example/osman/orders/TipActivity.java
@@ -3,9 +3,9 @@ package com.example.osman.orders;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -42,12 +42,12 @@ public class TipActivity extends AppCompatActivity {
     private void checkTips(Storage storage,ArrayList<Tip> tips) throws IOException, ParseException {
         Date date =  new Date(Long.parseLong(storage.getDate()));
         Log.d(storage.TAG,"checkTips | last date: " + date.toString());
-        long readyTip = DateUtils.getDateDiff(date,new Date(), TimeUnit.HOURS);
-        Toast.makeText(this,"READY TIP: " + readyTip,Toast.LENGTH_SHORT).show();
+        long readyTip = DateUtils.getDateDiff(date,new Date(), TimeUnit.DAYS);
+//        Toast.makeText(this,"READY TIP: " + readyTip,Toast.LENGTH_SHORT).show();
         Log.d(Storage.TAG,"ready tip : " + readyTip);
-        Toast.makeText(this,"READY TIP: " + readyTip,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this,"READY TIP: " + readyTip,Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(this,"PARSED DATE: " + date.toString(),Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this,"PARSED DATE: " + date.toString(),Toast.LENGTH_SHORT).show();
         if(readyTip<1) return;
         for (int i = 0;(i<tips.size()) && (readyTip>=1);i++){
             if(!tips.get(i).isOpen() && (!tips.get(i).isCanOpen())){
